0298bfdf42244c1e2f055af59e7b348821a2e82e
Joe Bain <joseph.bain@omgplc.com>
2016-01-30 13:29:43 +0000

    starting on portals

---
+++ Assets/Scripts/AreaManager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AreaManager : MonoBehaviour
+{
+    public string BaseScene = "main";
+    public string FirstScene = "inside-hut";
+
+    public static AreaManager instance;
+
+    void Start()
+    {
+        instance = this;
+
+        Scene[] allScenes = SceneManager.GetAllScenes();
+        foreach (Scene scene in allScenes)
+        {
+            if (scene.name != BaseScene && scene.isLoaded)
+            {
+                Debug.Log("unloading scene " + scene.name);
+                bool wasUnloaded = SceneManager.UnloadScene(scene.buildIndex);
+                Debug.Log("success? " + wasUnloaded);
+            }
+        }
+
+        if (!SceneManager.GetSceneByName(FirstScene).isLoaded)
+        {
+            Debug.Log("loading first scene " + FirstScene);
+           // SceneManager.LoadScene(FirstScene, LoadSceneMode.Additive);
+        }
+
+    }
+
+    public static AreaManager Get()
+    {
+        return instance;
+    }
+
+    public void ChangeArea(string sceneName)
+    {
+        Debug.Log("change area to " + sceneName);
+
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+
+        Scene[] allScenes = SceneManager.GetAllScenes();
+        foreach (Scene scene in allScenes)
+        {
+            if (scene.name != BaseScene && scene.name != sceneName && scene.isLoaded)
+            {
+                SceneManager.UnloadScene(scene.buildIndex);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Portal : MonoBehaviour {
+
+    public string SceneToLoad;
+    
+    void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("on trigger enter");
+        Player player = collider.GetComponent<Player>();
+        if (player != null)
+        {
+            AreaManager.Get().ChangeArea(SceneToLoad);
+        }
+    }
+}
