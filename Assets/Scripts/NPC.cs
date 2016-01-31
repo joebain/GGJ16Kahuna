94f06d1fd7962fd9a08384a3a2c15f42c9d36fd9
Joe Bain <joseph.bain@omgplc.com>
2016-01-31 13:02:20 +0000

    added crusoe. he walks around

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    private NavMeshAgent navAgent;
+
+    public bool Walking = false;
+
+    private Vector3 startPoint;
+
+    public void Start()
+    {
+        navAgent = GetComponent<NavMeshAgent>();
+
+        startPoint = transform.position;
+    }
+
+    public void Update()
+    {
+        if (Walking)
+        {
+            if (navAgent.isOnNavMesh && navAgent.remainingDistance <= navAgent.stoppingDistance)
+            {
+                navAgent.destination = startPoint + Random.insideUnitSphere * 20f;
+            }
+        }
+    }
+}
