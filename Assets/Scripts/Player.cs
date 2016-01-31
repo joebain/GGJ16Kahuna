d4e39949594e9efe753cb15f33506b288ce95461
Alex May <psychicteeth@gmail.com>
2016-01-31 10:28:41 +0000

    Prevented the player from moving while the text box is active. Changed the scaling of the canvas.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -39,6 +39,7 @@ public class Player : MonoBehaviour
 
     public void GoTo(Vector3 position)
     {
+		if (textBox.activeSelf) return;
         if (float.IsInfinity(position.x)) { return; }
         agent.destination = position;
         targetIndicator.transform.position = position;
